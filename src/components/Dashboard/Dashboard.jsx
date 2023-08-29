3e2c8312078af00c545d1d8a9b39b052889e43d9
Muhammad Talha Rehman <miantalhapaf@gmail.com>
2023-08-29 14:28:37 +0500

    QA Points Fixed

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -10,7 +10,7 @@ function Dashboard() {
   return (
     <>
       <div className='bgBlur overflow-hidden xl:block sm:hidden xs:hidden'></div>
-        <div className='grid grid-cols-12  2xl:ml-[10.813rem] lg:ml-20 xl:ml-[10rem] sm:ml-14   xs:m-0'>
+        <div className='grid grid-cols-12  2xl:ml-[10.813rem] lg:ml-20 xl:ml-[8rem] sm:ml-14   xs:m-0'>
           <div className='2xl:mt-[14.375rem] xl:mt-[9.375rem] lg:mt-20 sm:mt-[3.125rem] xs:mx-5 xs:mt-14 2xl:col-span-4 xl:col-span-6 lg:col-span-7 sm:col-span-12 xs:col-span-12'>
           <h4 className='bg-gradient-to-t from-[#2B3087] to-[#00B4EC] bg-clip-text text-transparent xs:ml-1 xs:text-sm ml-[3px]'>At Tokners we are</h4>
           <h2 className='text-5xl font-bold leading-[54px] tracking-[-0.06rem] text-[#fff] 2xl:w-[97%] xl:w-[562px] mt-[0.625rem] xs:text-4xl xs:leading-[40px]'>Reimagining social media through the power of the blockchain.</h2>
